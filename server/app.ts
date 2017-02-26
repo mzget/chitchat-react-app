374af93788a765bf3dabb7072dd43296bfe48774
Nattapon Rattajak <nattapon.r@live.com>
2017-02-26 19:07:15 +0700

    update demo#5

--- server/app.ts
+++ server/app.ts
@@ -9,7 +9,7 @@ const cors = require("cors");
 const useragent = require("express-useragent");
 import jwt = require("jsonwebtoken");
 
-process.env.NODE_ENV = `development`;
+process.env.NODE_ENV = `production`;
 const app = express();
 if (app.get("env") == "development") {
     process.env.PORT = 9000;
