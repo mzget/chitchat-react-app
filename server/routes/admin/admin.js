159612b632fa464a5a39044dab2c7a7e185c8ca3
Nattapon Rattajak <nattapon.r@live.com>
2017-04-03 23:42:51 +0700

    remove server code…

--- server/routes/admin/admin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require("express");
-const router = express.Router();
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    next();
-});
-module.exports = router;
