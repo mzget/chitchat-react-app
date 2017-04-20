7ec35d070754ac0c43b8b63194e2e5231dc3e45b
Fiiz Thanawat <fiizx.winchester@gmail.com>
2017-04-20 11:44:20 +0700

    add general style

---
+++ src/styles/generalStyles.js
@@ -0,0 +1,34 @@
+
+export const generalStyles = {
+  
+  paddingTop2Percent: {
+    "paddingTop": "2%"
+  },
+  paddingLeft2Percent: {
+    "paddingLeft": "2%"
+  },
+  paddingRight1Percent: {
+    "paddingRight": "1%"
+  },
+
+  paddingZero: {
+    padding: "0"
+  },
+
+  flexCenter: {
+    "display": "flex",
+    "justifyContent": "center",
+    "alignItems": "center"
+  },
+  flexStart: {
+    "display": "flex",
+    "justifyContent": "flex-start",
+    "alignItems": "center"
+  },
+
+  profileLine: {
+    "height": "45px",
+    "borderTop": "1px solid grey",
+    "borderBottom": "1px solid grey",
+  }
+}
