14a10c66ef8e26e5f85d40371361d7545b7a8fc8
Nattapon Rattajak <rattajak.n@gmail.com>
2017-05-30 04:52:48 +0700

    upgraded to create-react-app v.1.3.+

---
+++ src/styles/generalStyles.js
@@ -0,0 +1,71 @@
+
+export const generalStyles = {
+  
+  paddingTop2Percent: {
+    "paddingTop": "2%"
+  },
+  paddingLeft2Percent: {
+    "paddingLeft": "2%"
+  },
+  paddingRight1Percent: {
+    "paddingRight": "1%"
+  },
+
+  paddingZero: {
+    padding: "0"
+  },
+
+  flexCenter: {
+    "display": "flex",
+    "justifyContent": "center",
+    "alignItems": "center"
+  },
+  flexStart: {
+    "display": "flex",
+    "justifyContent": "flex-start",
+    "alignItems": "center"
+  },
+  flexEnd: {
+    "display": "flex",
+    "justifyContent": "flex-end",
+    "alignItems": "center"
+  },
+
+  gridCenter: {
+    display: "grid",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  profileLine: {
+    "height": "45px",
+    "borderTop": "1px solid grey",
+    "borderBottom": "1px solid grey",
+  },
+
+  pageBox: {
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 0px 0px 0 rgba(0, 0, 0, 0.19)",
+
+  },
+
+  marginTopBottom5Percent:{
+    marginTop: "5%",
+    marginBottom: "5%"
+  },
+  marginTop5Percent:{
+    marginTop: "5%",
+  },
+  marginTop2Percent:{
+    marginTop: "2%",
+  },
+  marginTop1Percent:{
+    marginTop: "1%",
+  },
+  margin2Percent: {
+    margin: "2%"
+  }, 
+  heightFull: {
+    height: "100%"
+  }
+  
+}
---
+++ src/styles/profile.js
@@ -0,0 +1,23 @@
+
+export const Styles = {
+
+  
+  boxShadow: {
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+  },
+
+  profileBox: {
+    margin: "1%", 
+    height: "40px",
+    
+  },
+
+  dropdownBox: {
+    margin: "1%", 
+    height: "65px",    
+  },
+
+  manageUserBox: {
+    marginTop: "2%"
+  }
+}
