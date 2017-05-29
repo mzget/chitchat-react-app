6eac2a9ec147446f382aeb64a96847063ca33115
Nattapon Rattajak <rattajak.n@gmail.com>
2017-05-30 05:30:23 +0700

    fixed : file name bug.

--- src/chitchat/chats/ChatslogComponent.js
+++ src/chitchat/chats/ChatslogComponent.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    var _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
     return { next: verb(0), "throw": verb(1), "return": verb(2) };
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
@@ -120,13 +120,13 @@ var ChatsLogComponent = (function () {
         async.each(roomAccess, function (item, resultCallback) {
             self.getRoomInfo(item.roomId)
                 .then(function (room) {
-                results.push(room);
-                resultCallback();
-            })["catch"](function (err) {
-                if (err)
-                    console.warn("getRoomInfo", err);
-                resultCallback();
-            });
+                    results.push(room);
+                    resultCallback();
+                })["catch"](function (err) {
+                    if (err)
+                        console.warn("getRoomInfo", err);
+                    resultCallback();
+                });
         }, function (err) {
             console.log("onAccessRoom.finished!", err);
             done();
