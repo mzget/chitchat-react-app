38d95dace2a094179719cbccf54faf1a4270bf77
Nattapon Rattajak <rattajak.n@gmail.com>
2017-07-19 16:46:36 +0700

    build....

--- src/Chitchat.js
+++ src/Chitchat.js
@@ -5,7 +5,7 @@ const stalkPort = 3050;
 const stalkKey = "chitchat1234";
 const chitchat_api = "https://chitchats.ga"; //"http://localhost:9000"; "https://chitchats.ga"
 export const chitchat_graphql = "https://chitchats.ga/graphql"; // "http://localhost:9000/graphql" : "https://chitchats.ga/graphql";
-export const signalingServer = "http://chitchats.ga:8888";
+export const signalingServer = "https://chitchats.ga:8888";
 export const config = {
     Stalk: {
         apiKey: stalkKey,
