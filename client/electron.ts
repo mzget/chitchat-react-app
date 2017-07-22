a9ac91a48eac167b1533c3f0c8f69a8cd676efb3
Nattapon Rattajak <rattajak.n@gmail.com>
2017-07-22 17:47:36 +0700

    build...

--- client/electron.ts
+++ client/electron.ts
@@ -2,14 +2,14 @@ const electron = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   // Create the browser window.
